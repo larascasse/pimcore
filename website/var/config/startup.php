42607b245fe41d44b958ecc8c099ef8ab0aeeac0
Florent Berenger <florent@lesmecaniques.net>
2016-12-12 17:02:11 +0100

    ise en place cloudfront pour le frontend

--- website/var/config/startup.php
+++ website/var/config/startup.php
@@ -12,5 +12,48 @@ if( !isset($_SERVER['PHP_AUTH_USER']) )
     }
 }
 
+//$s3AssetUrlPrefix = "//media.laparqueterienouvelle.fr";
+
+$cloudFrontPrefix = "//media.laparqueterienouvelle.fr";
+
+if (!\Pimcore::inAdmin() && !\Pimcore\Tool::isFrontentRequestByAdmin()) {
+    \Pimcore::getEventManager()->attach([
+        "frontend.path.asset.image.thumbnail",
+        "frontend.path.asset.document.image-thumbnail",
+        "frontend.path.asset.video.image-thumbnail",
+        "frontend.path.asset.video.thumbnail",
+    ],
+        function ($event) use ($cloudFrontPrefix) {
+            // rewrite the path for the frontend
+            $fileSystemPath = $event->getParam("filesystemPath");
+            $target = $event->getTarget();
+            $fileModTime = null;
+            if($target instanceof \Pimcore\Model\Asset) {
+                $fileModTime = $target->getModificationDate();
+            } elseif (method_exists($target, "getAsset") && $target->getAsset()) {
+                $fileModTime = $target->getAsset()->getModificationDate();
+            } elseif (file_exists($fileSystemPath)) {
+                $fileModTime = filemtime($fileSystemPath);
+            }
+ 
+            $path = str_replace(PIMCORE_DOCUMENT_ROOT, "", $fileSystemPath);
+            if($fileModTime) {
+                $path = "/cache-buster-" . $fileModTime . $path; // add a cache-buster
+            }
+            $path = $cloudFrontPrefix . $path;
+ 
+            return $path;
+        });
+ 
+    \Pimcore::getEventManager()->attach("frontend.path.asset", function ($event) use ($cloudFrontPrefix) {
+        $asset = $event->getTarget();
+        $path = $asset->getRealFullPath();
+        $path = "/cache-buster-" . $asset->getModificationDate() . $path; // add a cache-buster
+        $path = $cloudFrontPrefix . $path;
+ 
+        return $path;
+    });
+}
+
 $autoloader = Zend_Loader_Autoloader::getInstance();
 $autoloader->registerNamespace('Website');

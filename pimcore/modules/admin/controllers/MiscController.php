971871b78b4c0ebec1c73c00f5f5f159b684665b
Florent Berenger <florent@lesmecaniques.net>
2017-01-06 18:09:39 +0100

    debug script proxy

--- pimcore/modules/admin/controllers/MiscController.php
+++ pimcore/modules/admin/controllers/MiscController.php
@@ -89,11 +89,11 @@ class Admin_MiscController extends \Pimcore\Controller\Action\Admin
             $contentType = "text/css";
         }
 
-        $lifetime = 86400;
-        $this->getResponse()->setHeader("Cache-Control", "max-age=" . $lifetime, true);
-        $this->getResponse()->setHeader("Pragma", "", true);
+       // $lifetime = 86400;
+        //$this->getResponse()->setHeader("Cache-Control", "max-age=" . $lifetime, true);
+        //$this->getResponse()->setHeader("Pragma", "", true);
         $this->getResponse()->setHeader("Content-Type", $contentType, true);
-        $this->getResponse()->setHeader("Expires", gmdate("D, d M Y H:i:s", time() + $lifetime) . " GMT", true);
+        //$this->getResponse()->setHeader("Expires", gmdate("D, d M Y H:i:s", time() + $lifetime) . " GMT", true);
 
         echo $scriptsContent;
     }

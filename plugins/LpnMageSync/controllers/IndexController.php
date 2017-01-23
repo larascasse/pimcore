231b89d2ef5c5ad407b47e4d3e75e3933d866b14
Florent Berenger <florent@lesmecaniques.net>
2017-01-23 17:50:18 +0100

    Jout Plugin MageSync

---
+++ plugins/LpnMageSync/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class LpnMageSync_IndexController extends \Pimcore\Controller\Action\Admin
+{
+    public function indexAction()
+    {
+
+        // reachable via http://your.domain/plugin/LpnMageSync/index/index
+    }
+}

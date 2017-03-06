83c61e82095c0a970aff0147568b9069cc77c480
Forent B <florent@lesmecaniques.net>
2017-03-06 15:56:26 +0000

    OK

--- website/var/classes/definition_projectCategory.php
+++ website/var/classes/definition_projectCategory.php
@@ -1,7 +1,7 @@
 <?php 
 
 /** 
-* Generated at: 2017-02-02T14:50:29+01:00
+* Generated at: 2017-02-23T15:18:58+01:00
 * Inheritance: no
 * Variants: no
 * Changed by: florent (6)
@@ -9,6 +9,8 @@
 
 
 Fields Summary: 
+- localizedfields [localizedfields]
+-- name [input]
 */ 
 
 
@@ -16,7 +18,7 @@ return Pimcore\Model\Object\ClassDefinition::__set_state(array(
    'name' => 'projectCategory',
    'description' => '',
    'creationDate' => 0,
-   'modificationDate' => 1486043429,
+   'modificationDate' => 1487859538,
    'userOwner' => 6,
    'userModification' => 6,
    'parentClass' => '',
@@ -42,6 +44,84 @@ return Pimcore\Model\Object\ClassDefinition::__set_state(array(
      'permissions' => NULL,
      'childs' => 
     array (
+      0 => 
+      Pimcore\Model\Object\ClassDefinition\Layout\Panel::__set_state(array(
+         'fieldtype' => 'panel',
+         'labelWidth' => 100,
+         'layout' => NULL,
+         'name' => 'Layout',
+         'type' => NULL,
+         'region' => NULL,
+         'title' => NULL,
+         'width' => NULL,
+         'height' => NULL,
+         'collapsible' => NULL,
+         'collapsed' => NULL,
+         'bodyStyle' => NULL,
+         'datatype' => 'layout',
+         'permissions' => NULL,
+         'childs' => 
+        array (
+          0 => 
+          Pimcore\Model\Object\ClassDefinition\Data\Localizedfields::__set_state(array(
+             'fieldtype' => 'localizedfields',
+             'phpdocType' => '\\Pimcore\\Model\\Object\\Localizedfield',
+             'childs' => 
+            array (
+              0 => 
+              Pimcore\Model\Object\ClassDefinition\Data\Input::__set_state(array(
+                 'fieldtype' => 'input',
+                 'width' => NULL,
+                 'queryColumnType' => 'varchar',
+                 'columnType' => 'varchar',
+                 'columnLength' => 190,
+                 'phpdocType' => 'string',
+                 'regex' => '',
+                 'name' => 'name',
+                 'title' => 'Nom',
+                 'tooltip' => '',
+                 'mandatory' => false,
+                 'noteditable' => false,
+                 'index' => true,
+                 'locked' => NULL,
+                 'style' => '',
+                 'permissions' => NULL,
+                 'datatype' => 'data',
+                 'relationType' => false,
+                 'invisible' => false,
+                 'visibleGridView' => true,
+                 'visibleSearch' => true,
+              )),
+            ),
+             'name' => 'localizedfields',
+             'region' => NULL,
+             'layout' => NULL,
+             'title' => '',
+             'width' => '',
+             'height' => '',
+             'maxTabs' => NULL,
+             'labelWidth' => NULL,
+             'referencedFields' => 
+            array (
+            ),
+             'tooltip' => NULL,
+             'mandatory' => NULL,
+             'noteditable' => NULL,
+             'index' => NULL,
+             'locked' => NULL,
+             'style' => NULL,
+             'permissions' => NULL,
+             'datatype' => 'data',
+             'columnType' => NULL,
+             'queryColumnType' => NULL,
+             'relationType' => false,
+             'invisible' => false,
+             'visibleGridView' => true,
+             'visibleSearch' => true,
+          )),
+        ),
+         'locked' => NULL,
+      )),
     ),
      'locked' => NULL,
   )),

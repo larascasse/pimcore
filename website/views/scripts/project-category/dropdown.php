9fbf3f4825ccd4999e54f891c83bad7bc897ad71
Florent Berenger <florent@lesmecaniques.net>
2017-03-28 16:04:34 +0200

    Tous les projets

--- website/views/scripts/project-category/dropdown.php
+++ website/views/scripts/project-category/dropdown.php
@@ -6,6 +6,10 @@ $defaultName= isset($this->category)?$this->category->getName():"Tous les projet
   <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $defaultName ?></a>
     <div class="dropdown-menu">
+    <?php if (!isset($this->category)) { ?>
+      <a class="dropdown-item" href="/realisations-parquet-design">Tous les projets</a>
+    <?php } ?>
+    
     <?php foreach ($this->categories as $allCategory) { 
         ?>
       <a class="dropdown-item <?php echo (isset($this->category) && $allCategory->getId()== $this->category->getId())?"active":"";?>" href="/projets/category/<?php echo $allCategory->getKey() ?>"><?php echo $allCategory->getName() ?></a>

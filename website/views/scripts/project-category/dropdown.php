bdc4a25fcbd8385cdad1c5c10585eb622be00ec0
Florent Berenger <florent@lesmecaniques.net>
2017-03-28 16:13:53 +0200

    OOPS

--- website/views/scripts/project-category/dropdown.php
+++ website/views/scripts/project-category/dropdown.php
@@ -6,7 +6,7 @@ $defaultName= isset($this->category)?$this->category->getName():"Tous les projet
   <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $defaultName ?></a>
     <div class="dropdown-menu">
-    <?php if (!isset($this->category)) { ?>
+    <?php if (isset($this->category)) { ?>
       <a class="dropdown-item" href="/realisations-parquet-design">Tous les projets</a>
     <?php } ?>
     

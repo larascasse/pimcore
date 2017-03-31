98bdbaae10ff39521e4946392c9c4b8a33543af8
Florent Berenger <florent@lesmecaniques.net>
2017-03-31 11:39:30 +0200

    Maj gridview car

--- website/views/areas/lpn-home-grid/view.php
+++ website/views/areas/lpn-home-grid/view.php
@@ -9,7 +9,15 @@ if($this->editmode) {
         Nombre de colonnes: <?= $this->select("columns", [
             "width" => 60,
             "reload" => true,
-            "store" => [[1,1],[2,2],[3,3]]
+            "store" => [[1,1],[2,2],[3,3],[4,4],[5,5],[6,6]]
+        ]); ?>
+    </div>
+
+    <div class="container" style="padding-bottom: 40px">
+        Type: <?= $this->select("cardClass", [
+            "width" => 100,
+            "reload" => true,
+            "store" => [['rollbloc',"Texte sur photo, roll"],['card','Text en dessous']]
         ]); ?>
     </div>
 
@@ -19,6 +27,11 @@ if($this->editmode) {
 $count = $this->select("columns")->getData();
 if(!$count) {
     $count = 1;
+}
+
+$defaultCardClass = $this->select("cardClass")->getData();
+if(!$defaultCardClass) {
+    $defaultCardClass = 'rollbloc';
 }
 
 ?>
@@ -32,22 +45,34 @@ if(!$count) {
 
 <?php 
 for($i=0; $i<$count; $i++) { 
+   
     $smallClass="";
-    $cardClass="rollbloc";
+    $cardClass=$defaultCardClass;
 
     switch ($count) {
         case 1:
            $pimcoreThimbClass = "magento_equigrid_h";
+           $smallClass="col";
+           break;
         
         break;
 
         case 2:
            $pimcoreThimbClass = "magento_equigrid_v";
+           $smallClass="col";
+            break;
+
+        case 4:
+            $pimcoreThimbClass = "magento_equigrid_h";
+            $smallClass="-sm-12 col-md-6";
+            $smallClass="col-12 col-sm-6 col-sm-3";
             break;
 
         case 3:
+        case 6:
+        case 5:
            $pimcoreThimbClass = "magento_equigrid_h";
-           $smallClass="-sm-12 col-md";
+           $smallClass="col-12 col-sm-12 col-md-4";
            $cardClass="card";
             break;
         
@@ -56,15 +81,24 @@ for($i=0; $i<$count; $i++) {
             break;
     }
 
+    // le dernier prend tout la place
+    if($i==($count-1) && $i>1) {
+        $smallClass="col col-sm col-md";
+    }
+
     ?> 
     <!--  item -->
-    <div class="col-12 col-sm<?php echo  $smallClass?> table-bloc-thumb">
-    <div class="<?= $cardClass?><?=  $classNoRoll ?>">
+    <div class="col<?php echo  $smallClass?> table-bloc-thumb">
+    
 
 
 <?php
 
-        echo $this->image("image".$i,array(
+    if($this->editmode) {
+
+    }
+
+    echo $this->image("image".$i,array(
         "thumbnail" => $pimcoreThimbClass,
         "class" => "img-fluid".$classNoRoll
         
@@ -72,9 +106,12 @@ for($i=0; $i<$count; $i++) {
     );
 
 ?>
-
+    <?php if ($cardClass != "card") : ?>
+        <div class="<?= $cardClass?><?=  $classNoRoll ?>">
         <div class="table-bloc-thumb-text<?=  $classNoRoll ?> nsg_container">
-            <div class="table-thumb-type"><span class="table-thumb-subtext"><?= $this->input("titre".$i, ["width" => 400,'placeholder'=>'titre']); ?></span></div> 
+            <div class="table-thumb-type">
+                <span class="table-thumb-subtext"><?= $this->input("titre".$i, ["width" => 400,'placeholder'=>'titre']); ?></span>
+            </div> 
             <div class="rollbloc_txt_over">
                 <div class="rollbloc_txt_over_cnt">
                     <div class="rollbloc_txt"><?= $this->textarea("description".$i, ["width" => 600,'placeholder'=>'Description',"htmlspecialchars"=>false]); ?></div>
@@ -104,7 +141,41 @@ for($i=0; $i<$count; $i++) {
             ?>
 
          </div>
-    </div>
+         </div>
+    
+
+    <?php else : ?>
+        <div class="<?= $cardClass?><?=  $classNoRoll ?> clickable">
+         <div class="card-block">
+            <div class="card-title"><?= $this->input("titre".$i, ["width" => 400,'placeholder'=>'titre']); ?></div>
+            <div class="card-text"><?= $this->textarea("description".$i, ["width" => 600,'placeholder'=>'Description',"htmlspecialchars"=>false]); ?></div>
+                    
+            <?php 
+            if ($this->editmode) { 
+              
+                echo $this->input("btn_title".$i, ["width" => 600, "placeholder"=>"Titre du bouton"]); 
+
+            }
+            
+            echo $this->renderlet("link".$i, array(
+                        "types"=>array("object"),
+                        "controller" => "content",
+                        "action" => "link-eshop-renderlet",
+                        "title" => "Glisser un doc, un produit, une categorie",
+                        "editmode" => $this->editmode,
+                        "previewmode" => $this->previewmode,
+                        "btn_title" => $this->input("btn_title".$i)
+             ));
+
+
+
+            
+            ?>
+
+        </div>
+     </div>
+
+    <?php endif; ?>
     </div>
     <!-- fin item -->
     <?php } ?>

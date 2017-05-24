021cabb3f7ef92b230c2f12c80d8ddf6e4c7f258
Florent Berenger <florent@lesmecaniques.net>
2017-05-24 16:47:03 +0200

    MAJ COULEURS

--- website/views/areas/lpn-fond-couleur/view.php
+++ website/views/areas/lpn-fond-couleur/view.php
@@ -1,11 +1,62 @@
 <?php
 
-$colorSelect = $this->select("color-bkg", [
+$colors=[
+    "#F6F5F2",
+    "#F3F4EF",
+    "#FCEFE9",
+    "#8C8782",
+    "#E4E4D2",
+    "#E8F3F3",
+    "#D1DFE0",
+
+    "#DAD7CB",
+    "#D0D5C1",
+    "#F2BFA9",
+    "#8C8782",
+    "#93924B",
+    "#A4CFCE",
+    "#487E85",
+
+    "#48B7C9",
+    "#505050",
+    "#7994A5",
+    "#3C6297",
+    "#065446",
+    "#027683",
+    "#DD5639",
+
+];
+?>
+
+<div class="row">
+<?php
+$i==0;
+foreach ($colors as $color2) {
+    ?>
+    <div class="col" style="height: 50px; display: block; background-color: <?php echo $color2?>">
+    <?php echo $color2 ?>
+    </div>
+   
+   <?php
+   if($i==(count($colors)-1))
+        echo "</div>";
+   else if(($i%7)==6) {
+         echo "</div>";
+         echo '<div class="row">';
+   }
+   
+
+   $i++;
+}
+
+$color = $this->input("color",["placeholder"=>"Couleur de fond"]);
+
+/*$colorSelect = $this->select("color-bkg", [
             "width" => 300,
             "reload" => false,
             "store" => [['jaune','jaune'],['vert','vert'],['bleu','bleu'],['rose','rose'],['color-a','color-a'],['color-b','color-b']]
         ]);
-
+*/
 
 $hauteurSelect = $this->select("hauteur-bkg", [
             "width" => 300,
@@ -51,18 +102,19 @@ $marginSelect = $this->select("margin-bkg", [
 //position left / right
 
 if($this->editmode) {
-    echo $colorSelect;
-    echo $hauteurSelect;
-    echo $largeurSelect;
-    echo $marginSelect;
+    //echo $colorSelect;
+    echo $color."<br />";
+    echo $hauteurSelect."<br />";
+    echo $largeurSelect."<br />";
+    echo $marginSelect."<br />";
 }
 
-$color = $colorSelect->getData();
+//$color = $colorSelect->getData();
 $hauteur = $hauteurSelect->getData();
 $largeur = $largeurSelect->getData();
 $margin = $marginSelect->getData();
 ?>
 <div class="grid-offset"></div>
-<div class="grid-bkg <?php echo $color?' grid-bkg-'.$color:''?><?php echo $hauteur?' grid-height-'.$hauteur:''?><?php echo $largeur?' grid-width-'.$largeur:''?><?php echo $margin?' grid-margin-'.$margin:''?>">
+<div class="grid-bkg <?php echo $hauteur?' grid-height-'.$hauteur:''?><?php echo $largeur?' grid-width-'.$largeur:''?><?php echo $margin?' grid-margin-'.$margin:''?>" style="background-color: <?php echo $color->getData()?>">
 <div></div>
 </div>

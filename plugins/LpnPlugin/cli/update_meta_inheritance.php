928e125cdf194d79cb850d309593078ec748074b
Florent Berenger <florent@lesmecaniques.net>
2017-10-04 18:31:48 +0200

    update meta douglas

---
+++ plugins/LpnPlugin/cli/update_meta_inheritance.php
@@ -0,0 +1,72 @@
+<?php
+
+
+include(dirname(__FILE__) . "/../../../pimcore/cli/startup.php");
+
+//this is optional, memory limit could be increased further (pimcore default is 1024M)
+ini_set('memory_limit', '1024M');
+ini_set("max_execution_time", "-1");
+
+//execute in admin mode
+define("PIMCORE_ADMIN", true);
+Pimcore::setAdminMode();
+Object_Abstract::setHideUnpublished(false);
+Object_Abstract::setGetInheritedValues(false);
+
+
+Pimcore_Model_Cache::disable();
+
+
+$conditionFilters = array("
+       o_path LIKE '/catalogue/_product_base__/00specials/mfdglg-douglas%'
+
+    ");
+
+
+$list = new Pimcore\Model\Object\Listing();
+$list->setUnpublished(true);
+$list->setCondition(implode(" AND ", $conditionFilters));
+//$list->setOrder("ASC");
+//$list->setOrderKey("o_id");
+
+
+$list->load();
+
+$objects = array();
+ echo "objects in list ".count($list->getObjects())."\n";
+//Logger::debug("objects in list:" . count($list->getObjects()));
+foreach ($list->getObjects() as $object) {
+    //echo "update ".$object->getName()."\n";
+    //COPIE DE SCIERGNER COURT
+    //$value  = ucfirst(strtolower($object->getValueForFieldName('name_scienergie_court')));
+
+    
+    $inheritance = Object_Abstract::doGetInheritedValues(); 
+    Object_Abstract::setGetInheritedValues(false); 
+    $value = $object->getValueForFieldName('meta_title');
+    $parentValue = $object->getParent()->getValueForFieldName('meta_title');
+    if(($value == $parentValue || $value=="AA") && strlen($value)>0 ) {
+        echo "--> nullify ".$object->getName()."  -----    $value <-> $parentValue\n\n";
+        $objectToSave = Object::getById($object->getId());
+        $values = array();
+        $values['meta_title']=null;
+        $objectToSave->setValues($values);
+
+
+        //$objectToSave->setPublished(true);
+        $objectToSave->save();
+    }
+
+    
+
+    
+    
+  
+
+    
+
+    Object_Abstract::setGetInheritedValues($inheritance); 
+
+}
+
+?>

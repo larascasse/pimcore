4967950424666410771cb03ac297a01af75832da
Florent Berenger <florent@lesmecaniques.net>
2017-10-30 18:57:38 +0100

    Maj Mauchamp cloient

--- website/views/scripts/mauchamp/mauchamp-client.php
+++ website/views/scripts/mauchamp/mauchamp-client.php
@@ -2,39 +2,7 @@
 
 $(document).ready(function() {
 	console.log("INIT");
-  $('#selectAllFt').on('click', function () {
-  	//console.log('KKKKKKKKK',$('input[type="checkbox"]', '.check-ft'))
-    if ($(this).hasClass('allChecked')) {
-        $('.check-ft').attr('checked', false);
-    } else {
-        $('.check-ft').attr('checked', true);
-    }
-    $(this).toggleClass('allChecked');
-  });
-  $('#selectAllPose').on('click', function () {
-  	//console.log('KKKKKKKKK',$('input[type="checkbox"]', '.check-ft'))
-    if ($(this).hasClass('allChecked')) {
-        $('.check-pose').attr('checked', false);
-    } else {
-        $('.check-pose').attr('checked', true);
-    }
-    $(this).toggleClass('allChecked');
-  });
-  $('#selectAllPhoto').on('click', function () {
-  	//console.log('KKKKKKKKK',$('input[type="checkbox"]', '.check-ft'))
-    if ($(this).hasClass('allChecked')) {
-        $('.check-photo').attr('checked', false);
-    } else {
-        $('.check-photo').attr('checked', true);
-    }
-    $(this).toggleClass('allChecked');
-  });
-
-  $('#selectAllFt').click();
-  $('#selectAllPose').click();
-  $('#selectAllPhoto').click();
-
-
+  
 });
 
 
@@ -48,33 +16,81 @@ function hidePleaseWait () {
         };
 
 
+// Extended disable function
+jQuery.fn.extend({
+    disable: function(state) {
+        return this.each(function() {
+            var $this = $(this);
+            if($this.is('input, button, textarea, select'))
+              this.disabled = state;
+            else
+              $this.toggleClass('disabled', state);
+        });
+    }
+});
 
 
-function sendEmail() {
+function sendEmail(target) {
   console.log("KK",$("#mailform"),$("#mailform").serialize());
+  var btn = $(target);
+  btn.disabled=true;
   showPleaseWait();
   $.ajax({
-     url : '/?controller=mauchamp&action=mauchamp-sendmail',
-     data: $("#mailform").serialize()+'&sendmail=true',
+     url : 'https://www.laparqueterienouvelle.fr/LPN/create_customer.php',
+     data: $("#mailform").serialize(),
      method : "POST",
      success: function (data) {
 
-       
-
             hidePleaseWait();
             alert(data);
+  btn.disabled=false;
+
+      },
+      error: function (transport) {
+        
+              btn.disabled=false;
+              console.log(transport);
+              alert(transport.statusText);
+              hidePleaseWait();
 
       }
+
   });
 }
 
 </script>
-<form id="mailform">
+
+  <div class="container" style="padding-top: 40px;">
+  <div class="row">
+    <div class="col-xs-12">
+      <form id="mailform">
+      <div class="text-center">
+        <div class="checkbox">
+          <label>
+          <input type="checkbox" class="btn" name="newsletter" value="1" /> Inscription Newsletter
+          </label>
+        </div>
+        <div class="form-group">
+          <input type="button" class="btn" name="button" onclick="sendEmail(this)" value="Créer un compte web" /><br />
+
+          <textarea  cols="50" rows="20" name="xml"><?php echo $this->xmlClient ?></textarea>
+
+
+        </div>
+      </div>
+
+      </form>
+<hr />
 
 <?php foreach ($this->client as $key => $value) {
   echo '<div class="form-group"><label>'.$key.' : </label> <input disabled value="'.$value.'" /></div>';
 }
 ?>
-<textarea disabled cols="50" rows="20"><?php echo $this->xmlClient ?></textarea>
- </form>
+
+
+
 <div id="pleasewaitmodal" class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true"><div class="modal-dialog modal-sm"><div class="modal-content">En cours de traitement</div></div></div>
+
+</div>
+</div>
+</div>

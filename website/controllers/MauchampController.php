67032035f959f25750f15d5cc914dd29fb281919
Florent Berenger <florent@lesmecaniques.net>
2017-11-09 18:06:25 +0100

    first commit merge PDF (non foncitojnnelm

--- website/controllers/MauchampController.php
+++ website/controllers/MauchampController.php
@@ -118,7 +118,8 @@ class MauchampController extends Action
             try {
                  //Generate PDF
                 $httpsource = implode(" ", $ftUrls);
-                $pdfContent = $pdfContent = \Website\Tool\Wkhtmltopdf::convert($httpsource);
+                $contentPdfPath = $pdfFile?$pdfFile:PIMCORE_SYSTEM_TEMP_DIRECTORY . "/" . uniqid() . ".pdf";
+                $pdfContent = $pdfContent = \Website\Tool\Wkhtmltopdf::convert($httpsource,$contentPdfPath);
             }
             catch (Exception $e) {
                 //echo $e->getMessage().$httpsource;exit;
@@ -130,6 +131,78 @@ class MauchampController extends Action
                 $at->type = "application/pdf";
                 $at->filename = "viste_lpn.pdf";
                 $at->disposition = Zend_Mime::DISPOSITION_ATTACHMENT;
+            }
+
+            $addCover = true;
+            if($addCOver) {
+                try {
+
+                    
+                    //Creation de la cover : 
+                    $coverPath = \Pimcore\Tool::getHttpData("http://pimcore.florent.local/?controller=mauchamp&action=cover-for-piece-commerciale-pdf",null,["xml"=>$this->getParam('xml'),'createfile'=>true]);
+
+                    /*
+                    $pdf2show = new Zend_Pdf();
+ // $pdfContent
+is the generated one
+ $pdf1 = Zend_Pdf::parse($pdfContent, 1); 
+ //
+cloning the page (a must do)
+ $template = clone $pdf1->pages[0]; 
+ //
+Creating the first page of the merged PDF with the previous content
+ $page1 =
+new Zend_Pdf_Page($template); 
+ // Adding this page to the final PDF
+
+$pdf2show->pages[] = $page1; 
+ // Loading the statif PDF
+ $pdf2 =
+Zend_Pdf::load('urlToYourPDF.pdf'); 
+ // cloning the page (a must do)
+
+$template2 = clone $pdf2->pages[0]; 
+ // Creating the second page of the
+merged PDF with the previous content
+ $page2 = new
+Zend_Pdf_Page($template2);
+ // Adding this page to the final PDF 
+
+$pdf2show->pages[] = $page2; 
+ sendToWebBrowser('title',
+$pdf2show->render());
+*/
+
+
+                    // LOAD PDF DOCUMENTS
+                    $pdf1 = Zend_Pdf::load($coverPath);
+                    $pdf2 = Zend_Pdf::load($contentPdfPath);
+                    // WE WILL MERGE OUR TWO PDF FILES INTO A NEW ZEND_PDF OBJECT
+                    $pdfMerged = new Zend_Pdf();
+
+                    // ADD ALL PAGES FROM THE FIRST PDF TO OUR NEW DOCUMENT
+                    foreach($pdf1->pages as $page){
+                      $clonedPage = clone $page;
+                      $pdfMerged->pages[] = $clonedPage;
+                    }
+                    // ADD ALL PAGES FROM THE SECOND PDF TO OUR NEW DOCUMENT
+                    foreach($pdf2->pages as $page){
+                      $clonedPage = clone $page;
+                      $pdfMerged->pages[] = $clonedPage;
+                    }
+                    unset($clonedPage);
+
+                    // SEND THE MERGED PDF DOCUMENT TO BROWSER
+                    //header('Content-type: application/pdf');
+                    //echo $pdfMerged->render();
+
+                    //save to a file
+                    $final = $pdfFile?$pdfFile:PIMCORE_SYSTEM_TEMP_DIRECTORY . "/" . uniqid() . ".pdf";
+                    $pdfMerged->save($final);   
+                }
+                catch (Exception $e) {
+                    echo $e->getMessage();exit;
+                 }
             }
 
 
@@ -195,6 +268,99 @@ class MauchampController extends Action
 
 
     }
+
+     //Fiche Produit
+    public function coverForPieceCommercialeAction() {
+        
+        $this->disableBrowserCache();
+        $front = \Zend_Controller_Front::getInstance();
+        $front->unregisterPlugin("Pimcore\\Controller\\Plugin\\Cache");
+        $front->unregisterPlugin("Pimcore\\Controller\\Plugin\\Targeting");
+
+        $this->view->layout()->setLayout("layout-ft");
+
+        //POST from sceinergie
+        $xml = $this->getParam('xml');
+        
+        if(isset($xml))
+            $data = $xml;
+        else
+            //$xml = $data = \Website\Tool\MauchampHelper::getDebugClient();
+            $data = \Website\Tool\MauchampHelper::getDebugOrder();
+        
+        
+        $order = \Website\Tool\MauchampHelper::parseOrder($data);
+
+        $this->view->products = $order["products"];
+        $this->view->missingProducts = $order["missingProducts"];
+        $this->view->transport = $order["transport"];
+        $this->view->orderDetail = $order["orderDetail"];
+        $this->view->xmlOrder = $xml;
+
+    }
+
+
+
+
+
+
+     //Fiche Produit
+    public function coverForPieceCommercialePdfAction() {
+        //$this->coverForPieceCommercialeAction();
+        //return;
+        
+        $this->disableBrowserCache();
+        $this->disableViewAutoRender();
+
+        $front = \Zend_Controller_Front::getInstance();
+        $front->unregisterPlugin("Pimcore\\Controller\\Plugin\\Cache");
+        $front->unregisterPlugin("Pimcore\\Controller\\Plugin\\Targeting");
+        
+
+        $this->view->layout()->setLayout("layout-ft");
+      
+
+        //POST from sceinergie
+        $xml = $this->getParam('xml');
+        
+        if(isset($xml))
+            $data = $xml;
+        else
+            //$xml = $data = \Website\Tool\MauchampHelper::getDebugClient();
+            $data = \Website\Tool\MauchampHelper::getDebugOrder();
+
+
+
+
+         $fileContent = \Pimcore\Tool::getHttpData("http://pimcore.florent.local/?controller=mauchamp&action=cover-for-piece-commerciale",null,["xml"=>$data]);
+
+
+         //$filepath = $tmpPdfFile = PIMCORE_SYSTEM_TEMP_DIRECTORY . "/" . uniqid() . ".pdf";
+         $pdfContent = \Website\Tool\Wkhtmltopdf::fromString($fileContent);
+        
+        $filename = "laparqueterrienouvelle.pdf";
+
+         $headers = [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'inline; filename=" . ' . $filename . '";"',
+                'Cache-Control' => 'private'
+            ];
+
+            foreach ($headers as $key => $value) {
+                 $this->getResponse()->setHeader($key,$value);
+                 Header($key.":".$value);
+            }
+           
+
+             echo $pdfContent;
+             exit;
+
+
+     
+      
+
+    }
+
 
     public function log($error,$error_type='') {
         echo $message;

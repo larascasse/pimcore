b064059e5e3610bf804e83d5a23c061fba5da270
Florent Berenger <florent@lesmecaniques.net>
2017-11-22 15:25:27 +0100

    reoops

--- website/views/scripts/mauchamp/mauchamp.php
+++ website/views/scripts/mauchamp/mauchamp.php
@@ -222,7 +222,7 @@ foreach ($this->missingProducts as $product) {
 
 <?php
 //pas de bouton si pas de prouits 
-if(!count($products)) : ?>
+if(!count($this->products)) : ?>
 <div class="row">
 	<div class="col-12 text-right">
     

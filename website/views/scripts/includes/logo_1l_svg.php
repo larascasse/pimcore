39e9d807ac6a059c5835d53d422ac99120619447
Florent Berenger <florent@lesmecaniques.net>
2017-11-22 11:07:03 +0100

    logo plus grand

--- website/views/scripts/includes/logo_1l_svg.php
+++ website/views/scripts/includes/logo_1l_svg.php
@@ -1,4 +1,4 @@
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="338" height="30" viewBox="0 0 11539 1024">
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="507" height="45" viewBox="0 0 11539 1024">
 <title></title>
 <g id="icomoon-ignore">
 </g>

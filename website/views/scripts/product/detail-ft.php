7217dd0f6483ddec0cee733220144afa550c0093
Florent Berenger <florent@lesmecaniques.net>
2017-11-23 16:25:20 +0100

    marquage CE que pour les parquets

--- website/views/scripts/product/detail-ft.php
+++ website/views/scripts/product/detail-ft.php
@@ -177,7 +177,7 @@ echo $htmlStd;
 
 
 
-if ($hasMarquageCe) {
+if ($hasMarquageCe && $product->isParquet()) {
 	echo "<h2>Déclaration de performance</h2>";
 	echo $htmlCe;
 	echo '<p class="small"><br /><br />Il n’existe pas de PV pour le classement au feu des parquets massifs et contrecollés. Les classements feu que nous indiquons sur nos fiches techniques et autres documents sont des classements dits « conventionnels », stipulés dans les DTU 51.11 (Pose flottante des parquets contrecollés) et 51.2 (Pose collée des parquets massifs) et selon la norme NF 14341+A1.</p>';

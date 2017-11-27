4bfba5a527d870fea34db4e2ee1d01ff99c8b3fe
Florent Berenger <florent@lesmecaniques.net>
2017-11-27 17:17:44 +0100

    Maj

--- website/models/Website/Product.php
+++ website/models/Website/Product.php
@@ -596,7 +596,7 @@ class Website_Product extends Object_Product {
 			'largeur', 'largeur_txt',
 			'price','price_1','price_2','price_3','price_4',
 			'getMage_categoryIds','no_stock_delay','gallery','re_skus','cs_skus','us_skus',
-			'rendement','accessoirepopin','colisage','nbrpp','leadtime','shipping_type',
+			'rendement','accessoirepopin','colisageXXXX','nbrpp','leadtime','shipping_type',
 			'longueur','longueur_max','longueur_min','longueur_txt',
 			'quantity_min','quantity_max','quantity_min_max_txt',
 			'extra_content1','extra_content2',

495e762b598c8bef69d3f2bc1e473aad23f5438e
Florent Berenger <florent@lesmecaniques.net>
2017-11-28 12:39:40 +0100

    OOPS

---
+++ website/views/scripts/product/export-product-tech.php
@@ -0,0 +1,78 @@
+<?php 
+
+
+
+$products = $this->products;
+$idx = 0;
+$header=array();
+$rows = array();
+foreach ($products as $product) {
+	 $row=array();
+	 
+
+	 if(strlen($product->ean)==0) {
+	 	continue;
+	 }
+	 
+	 $header[] = "Famille";
+	 $header[] = "EAN";
+	 $header[] = "Url";
+
+	 $row[] = $product->getCode();
+	 $row[] = $product->getEan();
+	 $row[] = 'https://pim.laparqueterienouvelle.fr/id/'.$product->getId();
+
+
+	 $caracteristiques =  $product->getCharacteristicsArray();
+	 foreach ($caracteristiques as $key => $value) {
+
+ 	
+
+ 			if(!is_array($value)) {
+ 				continue;
+ 			}
+
+ 			if(!isset($value["label"]))
+ 				continue;
+
+ 			if($idx==0) {
+ 				$header[] = ucfirst(trim($value["label"]));
+ 			}
+ 			
+ 			$content = $description = null;
+
+ 			if(isset($value["content"]) && strlen(trim($value["content"]))>0) {
+				$content = trim($value["content"]);
+ 			}
+
+			if(isset($value["description"]) && strlen(trim($value["description"]))>0) {
+				$description = trim($value["description"]);
+			}
+
+			if(isset($description)) {
+				//$html.= '<br />';
+				$row[] = ucfirst(trim($description));
+
+			}
+			else {
+				$row[] = ucfirst($content);
+			}
+
+			
+
+			
+			
+	}
+
+	$row["url"] = $product->getEan();
+	$rows[]  = $row;
+	$idx ++;
+}
+echo implode(";", $header)."\n";
+foreach ($rows as $row) {
+	echo implode(";", $row)."\n";
+}
+
+
+ 
+

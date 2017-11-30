4341d40b3c2b2777d551d2e9fa9d64eca276fa0b
Florent Berenger <florent@lesmecaniques.net>
2017-11-30 17:06:15 +0100

    getFullPath

--- website/models/Website/ProjectPost.php
+++ website/models/Website/ProjectPost.php
@@ -71,7 +71,7 @@ class ProjectPost extends Object\ProjectPost {
          $itemData["name"] = $this->getName();
          $itemData["content"] = $this->getContent();
          $itemData["description"] = $this->getDescription();
-         $itemData["posterImage"] = $this->getPosterImage()?$this->getPosterImage()->getThumbnail("content")->getHTML():"";
+         $itemData["posterImage"] = $this->getPosterImage()?$this->getPosterImage()->getThumbnail("content")->getFullPath():"";
          $itemData["sku"] = ($product=$this->getRelatedProduct())?$product->getSku():"";
 
          return $itemData;

1696f795eec6ab352c71b553dbb034a5ee1368d2
Florent Berenger <florent@lesmecaniques.net>
2017-12-05 16:45:12 +0100

    MAJ ZP

---
+++ plugins/LpnPlugin/cli/update_cc-zp.php
@@ -0,0 +1,245 @@
+<?php
+
+
+include(dirname(__FILE__) . "/../../../pimcore/cli/startup.php");
+
+//this is optional, memory limit could be increased further (pimcore default is 1024M)
+ini_set('memory_limit', '1024M');
+ini_set("max_execution_time", "-1");
+
+//execute in admin mode
+define("PIMCORE_ADMIN", true);
+Pimcore::setAdminMode();
+Object_Abstract::setHideUnpublished(false);
+Object_Abstract::setGetInheritedValues(false);
+
+
+Pimcore_Model_Cache::disable();
+
+
+$conditionFilters = array("
+       o_path LIKE '/catalogue/_product_base__/05contreco/tmp/cc-zp%'
+
+    ");
+
+
+$list = new Pimcore\Model\Object\Listing();
+$list->setUnpublished(true);
+$list->setCondition(implode(" AND ", $conditionFilters));
+//$list->setOrder("ASC");
+//$list->setOrderKey("o_id");
+
+
+$list->load();
+
+$objects = array();
+ echo "objects in list ".count($list->getObjects())."\n";
+//Logger::debug("objects in list:" . count($list->getObjects()));
+
+foreach ($list->getObjects() as $object) {
+
+
+    //echo "update ".$object->getName()."\n";
+    //COPIE DE SCIERGNER COURT
+    //$value  = ucfirst(strtolower($object->getValueForFieldName('name_scienergie_court')));
+
+    if(!($object instanceof Object_Product))
+        continue;
+    
+    $inheritance = Object_Abstract::doGetInheritedValues(); 
+    Object_Abstract::setGetInheritedValues(false); 
+
+
+    $scienergieCourt = $object->name_scienergie_court;
+    $scienergie = $object->name_scienergie;
+    $article = $object->code;
+    $parent = $object->getParent();
+
+    //echo $scienergieCourt." ".$object->getEan()."\n";
+
+    $save=true;
+
+    /*if(stristr($scienergieCourt, "hd")) {
+        $object->setSupport('HDF');
+         $save=true;
+    }
+    else if(stristr($scienergieCourt, "cp")) {*/
+        $object->setSupport('cp');
+        $save=true;
+    //}
+
+   
+
+    //Usée brosssé
+    /*
+    Brossé,brosse
+Brossé accentué, brosse accentue
+Brut,brut
+Brut de sciage, brut de sciage
+Vieilli rives abimées, vieilli rives abimees
+Usé,use
+*/
+    echo "\n$article ?";
+
+    $save = true;
+    //epaisseur
+
+    if(strlen($object->getEan())>0) {
+        
+        switch ($object->getEpaisseur()) {
+            case '12':
+                $object->setValue('support','cp');
+                $object->setEpaisseurUsure('3.2 mm');
+
+            case '14':
+                $object->setValue('support','Latté');
+                $object->setEpaisseurUsure('4 mm');
+                break;
+            case '15':
+                $object->setValue('support','Latté');
+                $object->setEpaisseurUsure('4 mm');
+                break;
+            case '21':
+                $object->setValue('support','cp peuplier');
+                $object->setEpaisseurUsure('6 mm');
+                break;
+            default:
+                # code...
+                break;
+        }
+
+        if(stristr($scienergie, "HUILE AQUA")) {
+            $parent->setValue('finition',"huile-aqua");
+        }
+        if(stristr($scienergie, "VERNIS AQUA")) {
+            $parent->setValue('finition',"Verni aqua");
+        }
+        else if(stristr($scienergie, "HUILE CIRE")) {
+            $parent->setValue('finition',"huile-cire");
+        }
+
+         if(stristr($object->getEan(), "614401")) {
+            $object->setValue('largeur_txt',"Largeurs panachées 71/148/182 mm");
+            $object->setValue("pimonly_name_suffixe",$object->getEpaisseur().", largeurs panachées 71/148/182,long.".$object->getLongueur()." mm");
+        } 
+        else if(stristr($object->getEan(), "215429")) {
+            $object->setValue('largeur_txt',"Largeurs panachées 92/148/189 mm");
+             $object->setValue("pimonly_name_suffixe",$object->getEpaisseur().", largeurs panachées 92/148/189,long.".$object->getLongueur()." mm");
+        }
+        else if(stristr($object->getEan(), "21557")) {
+            $object->setValue('largeur_txt',"Largeurs panachées 148/189/240 mm");
+             $object->setValue("pimonly_name_suffixe",$object->getEpaisseur().", largeurs panachées 148/189/240,long.".$object->getLongueur()." mm");
+        }
+        else {
+            $object->setValue("pimonly_name_suffixe",$object->pimonly_dimensions);
+        }
+
+    }
+
+
+
+
+
+   
+    //CONTEMPORAIN
+    if(stristr($article, "FMCHEG2")) {
+       
+         
+         $save=true;
+    }
+
+    
+
+    
+
+
+    
+   
+
+    if(strlen($object->getEan())>0) {
+       
+
+        /* $parent = $object->getParent();
+        //On force le titre si plusiqueurs matieres
+        if(stristr($parent->getChoixString()," ou ")) {
+            $object->setValue("pimonly_name_suffixe",$object->getChoixString()." "."support ".strtoupper($object->getSupport('cp'))." ".$object->pimonly_dimensions);
+            $parent->setValue('pimonly_name_suffixe',null);
+
+        }  
+        else {
+            $object->setValue("pimonly_name_suffixe","support ".strtoupper($object->getSupport('cp'))." ".$object->pimonly_dimensions);
+            $parent->setValue('pimonly_name_suffixe',$parent->getChoixString());
+        }*/
+
+        
+        $save=true;
+
+       
+        if(strlen($parent->name)>0) {
+            $parent->setValue('chanfreins',"2 ou rives abimées");
+            
+        } 
+        $parent->save();
+        
+
+        echo "\nEan:".$object->getEan()." - ".$object->getMage_name(). ' - https://pim.laparqueterienouvelle.fr'.$object->getPreviewUrl();
+        
+    }
+    else {
+        echo "\nArticle:".$object->getCode()." - ".$object->getMage_name(). ' - https://pim.laparqueterienouvelle.fr'.$object->getPreviewUrl();
+    }
+   // continue;
+
+   
+    if($save)
+        $object->save();
+
+
+    continue;
+    $values = array();
+    $objectToSave = Object::getById($object->getId());
+    foreach ($fieldsToClean as $key => $fieldName) {
+        # code...
+        
+        
+
+        $value = $object->getValueForFieldName($fieldName);
+        if(!($object->getParent() instanceof Website_Product)) {
+            $parentValue = $object->getParent()->getParent()->getValueForFieldName($fieldName);
+         
+        }
+        else {
+            $parentValue = $object->getParent()->getValueForFieldName($fieldName);
+        }
+
+        
+
+        if(($value == $parentValue || $value=="Terrasses en bois par La Parqueterie Nouvelle") && strlen($value)>0 ) {
+            echo "--> nullify $fieldName : ".$object->getSku()."  -----    $value <-> $parentValue\n";
+            
+            
+            $values[$fieldName]=null;
+            
+
+
+            //$objectToSave->setPublished(true);
+            
+        }
+   
+    }
+
+    if(count( $values)>0) {
+        $objectToSave->setValues($values);
+        //print_r($values);
+
+        echo "\n";
+        $objectToSave->save();
+    }
+    
+    
+
+    Object_Abstract::setGetInheritedValues($inheritance); 
+
+}
+
+?>

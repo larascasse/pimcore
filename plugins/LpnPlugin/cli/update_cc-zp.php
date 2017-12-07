d22b83c849e632344d83365850d3ef09e4f5540d
Florent Berenger <florent@lesmecaniques.net>
2017-12-07 10:59:47 +0100

    MAJ

--- plugins/LpnPlugin/cli/update_cc-zp.php
+++ plugins/LpnPlugin/cli/update_cc-zp.php
@@ -90,7 +90,7 @@ Usé,use
             case '12':
                 $object->setValue('support','cp');
                 $object->setEpaisseurUsure('3.2 mm');
-                $object->setValue('longueur_txt',"Longueurs variables de 1100 à 2200 mm, présence de demi-longueurs");
+                $object->setValue('longueur_txt',"Longueurs variables de 1100 à 2200 mm, présence de demi-lames de début");
 
 
             case '14':

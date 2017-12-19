c724b3291eaa5039a6f1c04a247b2d98518c30a1
Florent Berenger <florent@lesmecaniques.net>
2017-12-19 18:02:00 +0100

    PDF URL

--- website/controllers/MauchampController.php
+++ website/controllers/MauchampController.php
@@ -181,7 +181,13 @@ class MauchampController extends Action
                     if(isset($clonedPage))
                         unset($clonedPage);
 
-                    $pdfContent = $pdfMerged->render();
+                    if (($file = @fopen($pdfFile,'wb')) === false ) {
+                        //require_once 'Zend/Pdf/Exception.php';
+                        //throw new Zend_Pdf_Exception( "Can not open '$filename' file for writing." );
+                    }
+                    $pdfMerged->save($file);
+                    fclose($file);
+                    $pdfContent = file_get_contents($file);
                  }
 
 

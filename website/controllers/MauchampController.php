652741249224b2a133f126be1275a7c4114de81a
Florent Berenger <florent@lesmecaniques.net>
2017-12-20 17:37:08 +0100

    debug client

--- website/controllers/MauchampController.php
+++ website/controllers/MauchampController.php
@@ -41,8 +41,8 @@ class MauchampController extends Action
         if(isset($xml))
             $data = $xml;
         else
-            //$xml = $data = \Website\Tool\MauchampHelper::getDebugClient();
-            $data = \Website\Tool\MauchampHelper::getDebugOrder();
+            $xml = $data = \Website\Tool\MauchampHelper::getDebugClient();
+            //$data = \Website\Tool\MauchampHelper::getDebugOrder();
         
         if(\Website\Tool\MauchampHelper::isClientRequest($data)) {
             $client = \Website\Tool\MauchampHelper::parseClient($data);

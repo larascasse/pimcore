4222b78a27109ff970cef504212717786e9ccf3c
Florent Berenger <florent@lesmecaniques.net>
2018-01-16 11:08:12 +0100

    OPS

--- website/views/scripts/product/detail-ft.php
+++ website/views/scripts/product/detail-ft.php
@@ -179,7 +179,7 @@ echo $htmlStd;
 
 
 
-if ($hasMarquageCe && ($product->isParquet() || $this->isTerrasse())) {
+if ($hasMarquageCe && ($product->isParquet() || $product->isTerrasse())) {
 	echo "<h2>Déclaration de performance</h2>";
 	echo $htmlCe;
 	if($product->isParquet()) {

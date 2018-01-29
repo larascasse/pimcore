e5854522fd0583f0f5210aac9630592bf349acfa
Florent Berenger <florent@lesmecaniques.net>
2018-01-29 10:52:21 +0100

    Maj BW import

---
+++ plugins/LpnPlugin/cli/update_cc-bw.php
@@ -0,0 +1,127 @@
+<?php
+
+
+include(dirname(__FILE__) . "/../../../pimcore/cli/startup.php");
+
+//this is optional, memory limit could be increased further (pimcore default is 1024M)
+ini_set('memory_limit', '1024M');
+ini_set("max_execution_time", "-1");
+
+//execute in admin mode
+define("PIMCORE_ADMIN", true);
+Pimcore::setAdminMode();
+Object_Abstract::setHideUnpublished(false);
+Object_Abstract::setGetInheritedValues(false);
+
+
+Pimcore_Model_Cache::disable();
+\Pimcore\Model\Version::disable();
+
+$conditionFilters = array("
+       o_path LIKE '/catalogue/_product_base__/05contreco/tmp/cc-bw%'
+
+    ");
+
+
+$list = new Pimcore\Model\Object\Listing();
+$list->setUnpublished(true);
+$list->setCondition(implode(" AND ", $conditionFilters));
+//$list->setOrder("ASC");
+//$list->setOrderKey("o_id");
+
+
+$list->load();
+
+$objects = array();
+ echo "objects in list ".count($list->getObjects())."\n";
+//Logger::debug("objects in list:" . count($list->getObjects()));
+
+foreach ($list->getObjects() as $object) {
+
+
+    //echo "update ".$object->getName()."\n";
+    //COPIE DE SCIERGNER COURT
+    //$value  = ucfirst(strtolower($object->getValueForFieldName('name_scienergie_court')));
+
+    if(!($object instanceof Object_Product))
+        continue;
+    
+    $inheritance = Object_Abstract::doGetInheritedValues(); 
+    Object_Abstract::setGetInheritedValues(false); 
+
+
+    $scienergieCourt = $object->name_scienergie_court;
+    $scienergie = $object->name_scienergie;
+    $code = $article = $object->code;
+    $ean  = $object->ean;
+    $parent = $object->getParent();
+
+    //echo $scienergieCourt." ".$object->getEan()."\n";
+
+    $save=true;
+
+    
+    echo "\n$article ?";
+
+    $save = true;
+    //epaisseur
+
+    if(strlen($object->getEan())>0) {
+
+        $object->setChauffantBasseTemperature("1");
+                 $object->setChauffantRadiantElectrique("1");
+                 $object->setSolRaffraichissant("0");
+
+                 
+
+        switch ($object->getEpaisseur()) {
+
+            case '12':
+                // $object->setChauffantBasseTemperature("1");
+                // $object->setChauffantRadiantElectrique("1");
+                // $object->setSolRaffraichissant("1");
+                 break;
+
+            case '14':
+                 
+                 break;
+
+          
+            default:
+                # code...
+                break;
+        }
+
+
+    }
+    
+   
+
+    if(strlen($object->getEan())>0) {
+        $object->save();
+
+        //$parent->save();
+
+       
+       /* 
+
+        
+        */
+
+       echo "\nEan:".$object->getEan()." - ".$object->getMage_name(). ' - https://pim.laparqueterienouvelle.fr'.$object->getPreviewUrl();
+        
+    }
+    else {
+
+        //echo "\nArticle:".$object->getCode()." - ".$object->getMage_name(). ' - https://pim.laparqueterienouvelle.fr'.$object->getPreviewUrl();
+        //$object->save();
+
+    }
+   
+    
+
+    Object_Abstract::setGetInheritedValues($inheritance); 
+
+}
+\Pimcore\Model\Version::enable();
+?>

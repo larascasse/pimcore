7d3ee337e4307410d02b6a4cd66a7307342d72ee
Florent Berenger <florent@lesmecaniques.net>
2018-03-16 18:12:36 +0100

    maj

--- website/views/scripts/test/test-search.php
+++ website/views/scripts/test/test-search.php
@@ -159,7 +159,7 @@
 
 <div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" id="surface" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    surface
+    Surface
   </button>
   <div class="dropdown-menu" aria-labelledby="surface">
 	<a class="dropdown-item" href="#">Contemporaine</a>

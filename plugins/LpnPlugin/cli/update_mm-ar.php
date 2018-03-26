597472e9f705825b98288e819a607b317a5c421e
Florent Berenger <florent@lesmecaniques.net>
2018-03-27 00:12:40 +0200

    OPPQ

--- plugins/LpnPlugin/cli/update_mm-ar.php
+++ plugins/LpnPlugin/cli/update_mm-ar.php
@@ -342,7 +342,7 @@ foreach ($list->getObjects() as $object) {
 
     
     if($isDalle) {
-        $parent->setValue('name',str_replace('Parquet chêne massif ','Parquet Versailles en chêne massif',$parent->getName()));
+        $parent->setValue('name',str_ireplace('Parquet chêne massif ','Parquet Versailles en chêne massif',$parent->getName()));
     }
     else {
         $parent->setValue('name',null);

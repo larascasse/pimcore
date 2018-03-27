48230563d3efaf0568154e37b4a3cf1f0cadbeab
Florent Berenger <florent@lesmecaniques.net>
2018-03-27 18:02:34 +0200

    test teinte

---
+++ plugins/LpnPlugin/cli/insert_product_to_teinte.php
@@ -0,0 +1,78 @@
+<?php
+//http://pim.laparqueterienouvelle.fr/?controller=product&action=export-product-tech&path=/catalogue/_product_base__/01massif/tmp/mm-zp
+
+include(dirname(__FILE__) . "/../../../pimcore/cli/startup.php");
+
+//this is optional, memory limit could be increased further (pimcore default is 1024M)
+ini_set('memory_limit', '1024M');
+ini_set("max_execution_time", "-1");
+
+//execute in admin mode
+define("PIMCORE_ADMIN", true);
+Pimcore::setAdminMode();
+Object_Abstract::setHideUnpublished(false);
+
+
+
+Pimcore_Model_Cache::disable();
+\Pimcore\Model\Version::disable();
+
+$conditionFilters = array(
+    "o_path LIKE '/teintes/teintes/_import%'",
+    //"ean IS NOT NULL"
+);
+
+
+$list = new Pimcore\Model\Object\Teinte\Listing();
+$list->setUnpublished(true);
+$list->setCondition(implode(" AND ", $conditionFilters));
+//$list->setOrder("ASC");
+//$list->setOrderKey("o_id");
+
+//$list->setOrder("DESC");
+//$list->setOrderKey("o_id");
+
+
+$list->load();
+
+$objects = array();
+ echo "objects in list ".count($list->getObjects())."\n";
+//Logger::debug("objects in list:" . count($list->getObjects()));
+
+foreach ($list->getObjects() as $object) {
+
+
+    //echo "update ".$object->getName()."\n";
+    //COPIE DE SCIERGNER COURT
+    //$value  = ucfirst(strtolower($object->getValueForFieldName('name_scienergie_court')));
+
+    if(!($object instanceof Object_Teinte))
+        continue;
+    
+    $teinteName = $object->getName();
+
+    $conditionFilters = array(
+    "o_path LIKE '/catalogue/_product_base__/05contreco/tmp/cc-ar%'",
+    "ean IS NULL",
+    "code IS NOT NULL",
+    "name like '% ".$teinteName." %'"
+    );
+
+
+    $listProduct = new Pimcore\Model\Object\Product\Listing();
+    $listProduct->setUnpublished(true);
+    $listProduct->setCondition(implode(" AND ", $conditionFilters));
+
+    foreach ($listProduct->getObjects() as $product) {
+        echo $teinteName." ".$product->getName()."\n";
+
+    }
+
+
+
+
+
+
+}
+\Pimcore\Model\Version::enable();
+?>

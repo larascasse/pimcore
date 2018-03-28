7a1e4fcbc504e0571f2246717788db2bb4072f72
Florent Berenger <florent@lesmecaniques.net>
2018-03-28 17:51:50 +0200

    OPPS

--- website/lib/Website/Tool/Wkhtmltopdf.php
+++ website/lib/Website/Tool/Wkhtmltopdf.php
@@ -137,7 +137,7 @@ class Wkhtmltopdf {
         $cmd = $wkhtmltopdfBinary.$options." '" . $httpSource . "' '" . $tmpPdfFile."'";
         //echo $cmd; die;
             system( $cmd, $retVal);
-            Logger::log("Wkhtml mcmd : ".$cmd);
+            \Pimcore\Log\Simple::log("pdf.log","Wkhtml mcmd : ".$cmd);
 
               if ($retVal != 0 && $retVal != 1) {
                     echo "Lpn wkhtmltopdf reported error (" . $retVal . ")". "\ncommand was:" . $cmd;

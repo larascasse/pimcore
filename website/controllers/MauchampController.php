def3984b9244db0ca8b025650719124f138589ab
Florent Berenger <florent@lesmecaniques.net>
2018-04-06 16:48:29 +0200

    OOPS

--- website/controllers/MauchampController.php
+++ website/controllers/MauchampController.php
@@ -303,8 +303,8 @@ class MauchampController extends Action
                 // reachable via http://your.domain/plugin/LpnPlugin/index/index
                 $pimpampoum = new \LpnPlugin\Model\PimPamPoum();
                 $pimpampoum->setValue("type","pimpampoum-".$order["orderDetail"]["Type_Piece"]);
-                $pimpampoum->setXml($xml);
-                $pimpampoum->settoEmail($this->getParam("to-email"));
+                $pimpampoum->setXml($this->getParam('xml'));
+                $pimpampoum->setToEmail($this->getParam("to-email"));
                 $pimpampoum->setFromEmail($this->getParam("from-email"));
                 $pimpampoum->setCodePiece($order["orderDetail"]["Code_Commande"]);
                 $pimpampoum->setDate(time());

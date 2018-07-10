3783c1615222c9b77687874ce05959daeacbb22e
Florent Berenger <florent@lesmecaniques.net>
2018-07-10 16:40:07 +0200

    OPPS

--- plugins/LpnPlugin/cli/update_mm-pn.php
+++ plugins/LpnPlugin/cli/update_mm-pn.php
@@ -132,7 +132,7 @@ foreach ($list->getObjects() as $object) {
 
         if($longueur_min>0) {
             $object->setValue("pimonly_name_suffixe",$object->getEpaisseur()."x".$object->getlargeur()."x".$longueur_min."-".$longueur_max." ".($isVariable?"variables":"fixes")."");
-            $object->setValue("longueur_txt","Longeurs ".($isVariable?"variables":"fixes")." de ".$longueur_min." à ".$longueur_max." mm");
+            $object->setValue("longueur_txt","Longueurs ".($isVariable?"variables":"fixes")." de ".$longueur_min." à ".$longueur_max." mm");
         }
 
         $object->setValue('chanfreins',"2");

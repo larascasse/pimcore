70cc90e8449e0cdcf0ead3f90496c17bca328fc5
Florent Berenger <florent@lesmecaniques.net>
2018-10-22 15:47:08 +0200

    maj

--- website/views/scripts/product/print-depot-label.php
+++ website/views/scripts/product/print-depot-label.php
@@ -144,6 +144,9 @@ $httpFile = \Pimcore\Tool::getHostUrl() . str_replace($_SERVER["DOCUMENT_ROOT"],
 elseif (isset($order)) {
 	
 ?>
+<script type="text/javascript">
+	screen.lockOrientation('portrait');
+</script>
 <style type="text/css" media="print">
 	
 /* DYMO 

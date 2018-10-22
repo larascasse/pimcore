20b75725286c773cd6c5e7259299e64a53caaee4
Florent Berenger <florent@lesmecaniques.net>
2018-10-22 15:13:47 +0200

    grosse etiuette

--- website/views/scripts/product/print-depot-label.php
+++ website/views/scripts/product/print-depot-label.php
@@ -33,7 +33,7 @@ if (!isset($product) && !isset($order)) {
 <input type="hidden" name="controller" value="product" \>
 <input class="form-control form-control-lg" name="ean" type="text" placeholder="EAN" value="<?php echo isset($this->ean)?$this->ean:""?>">
 </div>
-<button type="submit" name="format"  value="small_label" class="btn btn-primary btn-lg">Petite etiquette</button>
+<button type="submit" name="format"  disabled value="small_label" class="btn btn-primary btn-lg">Petite etiquette</button>
 <button type="submit" name="format"  value="big_label" class="btn btn-primary btn-lg">Grosse etiquette</button>
 </form>
 </div>

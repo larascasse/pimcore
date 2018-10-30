774627a0489d26f354443e276b1ad9080800daa6
Florent Berenger <florent@lesmecaniques.net>
2018-10-30 10:46:00 +0100

    MÉLANGE

--- website/models/Website/Product.php
+++ website/models/Website/Product.php
@@ -3304,6 +3304,8 @@ Autrement dit, hors des cas particuliers cités, tous les parquets conviennent q
     	$short_name = str_ireplace("support ", " ", $short_name);
     	$short_name = str_ireplace("premier", "PR", $short_name);
     	$short_name = str_ireplace("rustique", "RU", $short_name);
+    	$short_name = str_ireplace("mélange ", "<br />", $short_name);
+    	$short_name = str_ireplace("MÉLANGE ", "<br />", $short_name);
 
 
     	//Terrasse

95d6a6c4a8c519f060943d4edcf99f8149490ae6
Florent Berenger <florent@lesmecaniques.net>
2018-11-09 10:41:28 +0100

    placage confiragiure_free_A pour collection

--- website/lib/Website/Tool/ProductHelper.php
+++ website/lib/Website/Tool/ProductHelper.php
@@ -20,7 +20,7 @@ class ProductHelper
        
     
         $childConfigurableFields = array();
-        $configurableFields =array("volume","subtype","finitionString","hauteur","profil","fixation","color","epaisseur","largeur","longueur","conditionnement","epaisseur_txt","largeur_txt","longueur_txt","mage_section","quantity_min_txt","configurable_free_1","configurable_free_2","choixString","traitement_surfaceString","motifString","supportString");
+        $configurableFields =array(,"configurable_free_1","volume","subtype","finitionString","hauteur","profil","fixation","color","epaisseur","largeur","longueur","conditionnement","epaisseur_txt","largeur_txt","longueur_txt","mage_section","quantity_min_txt","configurable_free_2","choixString","traitement_surfaceString","motifString","supportString");
         $latestChild = null;
 
         $ignoreFields = array();
@@ -182,7 +182,7 @@ class ProductHelper
         }
 
         //On ordonne suivant le tableau suivant :
-        $order = array('motifString','subtype','profil','fixation','choixString','finitionString','supportString','traitement_surfaceString','epaisseur','epaisseur_txt','largeur','largeur_txt','longueur','longueur_txt');
+        $order = array('motifString','subtype','configurable_free_1','profil','fixation','choixString','finitionString','supportString','traitement_surfaceString','epaisseur','epaisseur_txt','largeur','largeur_txt','longueur','longueur_txt');
 
 
 

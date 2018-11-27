b5ab287f056d4e1c912b33cb4f951e001ce997c3
Florent Berenger <florent@lesmecaniques.net>
2018-11-27 14:53:22 +0100

    Maj

--- website/views/areas/lpn-home-grid-destructure/view.php
+++ website/views/areas/lpn-home-grid-destructure/view.php
@@ -48,7 +48,7 @@ $isDoubleSize=true;
 
 $simple=false;
 
-$hasText = strlen(trim($main_titre))>0 && strlen(trim($main_description))>0;
+$hasText = strlen(trim($main_titre->getData()))>0 && strlen(trim($main_description->getData()))>0;
 $blocText = '<div class="table-bloc-thumb grid-bloc-text"><h2>'.$main_titre.'</h2><p>'.$main_description.'</p></div>';
 
 echo $blocText ;

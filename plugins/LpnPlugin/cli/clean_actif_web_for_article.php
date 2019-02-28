28d2220f83e39bf369f9e622aee60eb20fd7bd51
Florent Berenger <florent@lesmecaniques.net>
2019-02-28 17:14:34 +0100

    Pas d’actif web pour les articles, que pour les EAN

---
+++ plugins/LpnPlugin/cli/clean_actif_web_for_article.php
@@ -0,0 +1,64 @@
+<?php
+
+
+include(dirname(__FILE__) . "/../../../pimcore/cli/startup.php");
+
+//this is optional, memory limit could be increased further (pimcore default is 1024M)
+ini_set('memory_limit', '1024M');
+ini_set("max_execution_time", "-1");
+
+//execute in admin mode
+define("PIMCORE_ADMIN", true);
+Pimcore::setAdminMode();
+Object_Abstract::setHideUnpublished(false);
+//Object_Abstract::setGetInheritedValues(false);
+
+
+Pimcore_Model_Cache::disable();
+
+
+ $conditionFilters = array(
+    "( 
+         o_path LIKE '/catalogue/_product_base__/05contreco/tmp/%'
+        OR o_path LIKE '/catalogue/_product_base__/01massif/tmp/%'
+    )",
+    "ean IS NULL",
+    "code IS NOT NULL",
+    //"LOWER(name) like '% ".strtolower($teinteName)." %'"
+    );
+
+
+
+
+$list = new Pimcore\Model\Object\Product\Listing();
+$list->setUnpublished(true);
+
+$condition = implode(" AND ", $conditionFilters);
+echo $condition;
+$list->setCondition($condition );
+//$list->setOrder("ASC");
+//$list->setOrderKey("o_id");
+
+
+$list->load();
+
+$objects = array();
+ echo "objects in list ".count($list->getObjects()).'\n';
+//Logger::debug("objects in list:" . count($list->getObjects()));
+
+ foreach ($list->getObjects() as $object) {
+        
+        echo $object->actif_web?"Actif\n":"--\n";
+
+        if($object->actif_web) {
+            $object->actif_web = 0;
+            $object->save();
+        }
+    
+
+
+
+}
+
+
+?>

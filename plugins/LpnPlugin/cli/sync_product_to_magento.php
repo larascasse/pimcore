15736038742d5dcfcb97db3263fc71fbd080c43b
Florent Berenger <florent@lesmecaniques.net>
2019-03-13 11:30:15 +0100

    encore

--- plugins/LpnPlugin/cli/sync_product_to_magento.php
+++ plugins/LpnPlugin/cli/sync_product_to_magento.php
@@ -33,7 +33,7 @@ foreach ($results as $result) {
     //echo $result['ean']."\n";
     $eans[] = $result['ean'];
     $cids[] = $result['cid'];
-    if($i++>100)
+    if($i++>10)
       break;
   }
 }

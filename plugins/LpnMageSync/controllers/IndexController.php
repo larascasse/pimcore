92cca70fa11e0f122926050b513ee2bab40364db
Florent Berenger <florent@lesmecaniques.net>
2019-03-14 12:28:23 +0100

    OPPS

--- plugins/LpnMageSync/controllers/IndexController.php
+++ plugins/LpnMageSync/controllers/IndexController.php
@@ -19,101 +19,122 @@ class LpnMageSync_IndexController extends \Pimcore\Controller\Action\Admin
 
        	  $this->disableViewAutoRender();
 
-       	  $product = Object_Product::getById($this->getParam("id"));
-
-       	  $withChildren = $this->getParam("withChildren");
-       	  $configurable = $this->getParam("configurable");
           $teinte = $this->getParam("teinte");
 
-       	  $create = $this->getParam("create");
-
-       	  if(!$product instanceof Object_Product) {
-       	  	  $this->_helper->json->sendJson($this->response = array("message"=>"Produit inconnu"));
+          if($teinte) {
+            $product = Object_Product::getById($this->getParam("id"));
+             if(!$product instanceof Object_Product) {
+              $this->_helper->json->sendJson($this->response = array("message"=>"Produit inconnu"));
              return;
-       	  }
+            }
+          }
+          else {
+
+            $product = Object_Teinte::getById($this->getParam("id"));
+             if(!$product instanceof Object_Teinte) {
+              $this->_helper->json->sendJson($this->response = array("message"=>"Teinte inconnue"));
+             return;
+            }
+
+          }
        	  
-       	  else {
-              $url = "https://www.laparqueterienouvelle.fr/LPN/get_a_product_magmi.php";
+
+       	 
+
+       	 
        	  
-          //DEV
-          $url = "http://shopdev.laparqueterienouvelle.fr/LPN/get_a_product_magmi.php";
 
-	    	  $params = array();
 
-	    	  $params["time"] = time();
+            $url = "https://www.laparqueterienouvelle.fr/LPN/get_a_product_magmi.php";
+       	  
+            //DEV
+            $url = "http://shopdev.laparqueterienouvelle.fr/LPN/get_a_product_magmi.php";
 
-	    	  if($withChildren || $teinte) {
-	    	  	$params["path"] = $product->getFullPath();
 
-	    	  	
-	    	  	//else if($simple)
-	    	  	//	$params["simple"] = 1;
-	    	  	
-	    	  }
-	    	  else if(strlen($product->getEan())>0) {
-	    	  	$params["ean"] = $product->getEan();
-	    	  }
-	    	  else if(strlen($product->getCode())>0) {
-	    	  	$params["code"] = $product->getCode();
-	    	  }
+            $withChildren = $this->getParam("withChildren");
+            $configurable = $this->getParam("configurable");
+            
+            $create = $this->getParam("create");
 
-	    	  if($create) {
-	    	  	$params["create"] = 1;
-	    	  }
 
-          if($configurable)
-              $params["configurable"] = 1;
+  	    	  $params = array();
+
+  	    	  $params["time"] = time();
+
+  	    	  if($withChildren || $teinte) {
+  	    	  	$params["path"] = $product->getFullPath();
+
+  	    	  	
+  	    	  	//else if($simple)
+  	    	  	//	$params["simple"] = 1;
+  	    	  	
+  	    	  }
+  	    	  else if(strlen($product->getEan())>0) {
+  	    	  	$params["ean"] = $product->getEan();
+  	    	  }
+  	    	  else if(strlen($product->getCode())>0) {
+  	    	  	$params["code"] = $product->getCode();
+  	    	  }
+
+  	    	  if($create) {
+  	    	  	$params["create"] = 1;
+  	    	  }
+
+            if($configurable)
+                $params["configurable"] = 1;
 
             if($teinte)
               $params["teinte"] = 1;
-	    	
-	    	  $content = \Pimcore\Tool::getHttpData($url,$params);
-
-          // Pour le workflow management,
-          // On va mettre à jour tous le produits mpour dire qu'ils ont été synchronisés.
-          //TODO : on ne gere pas les erreur ...
-          $products = [$product];
 
 
-          if($withChildren && !$teinte) {
-            //On va chercher tous les enfants
-              $list = new Pimcore\Model\Object\Product\Listing();
-              $list->setCondition("o_path LIKE '" . $product->getRealFullPath() . "/%'");
-              //$productIds[] = "o_path LIKE '" . $relatedProduct->getRealFullPath() . "/%'";
-              $childrens = $list->load();
+  	    	
+  	    	  $content = \Pimcore\Tool::getHttpData($url,$params);
 
-              foreach ($childrens as $simpleProduct) {
+            // Pour le workflow management,
+            // On va mettre à jour tous le produits mpour dire qu'ils ont été synchronisés.
+            //TODO : on ne gere pas les erreur ...
+            $products = [$product];
 
-                  $products[] = $simpleProduct;
-              }
-          }
 
-          /*Logger::debug("Start wrokflow upfate",$products);
-          echo "Start wrokflow upfate".count($products).$products[2]->getMage_name();
-          die;*/
-          $returnValueContainer = new \Pimcore\Model\Tool\Admin\EventDataContainer(array());
+            if($withChildren && !$teinte) {
+              //On va chercher tous les enfants
+                $list = new Pimcore\Model\Object\Product\Listing();
+                $list->setCondition("o_path LIKE '" . $product->getRealFullPath() . "/%'");
+                //$productIds[] = "o_path LIKE '" . $relatedProduct->getRealFullPath() . "/%'";
+                $childrens = $list->load();
 
-          \Pimcore::getEventManager()->trigger('lpn.magento.postSynchro',$products,[
-                "returnValueContainer" => $returnValueContainer
-            ]);
+                foreach ($childrens as $simpleProduct) {
 
-          $workflowReturn = $returnValueContainer->getData();
+                    $products[] = $simpleProduct;
+                }
+            }
 
-          if(is_array($workflowReturn) && isset($workflowReturn["message"])) {
-            $content .= $workflowReturn["message"];
-          }
-	    	
-	    	  $this->response = array(
-	    	  	"success" => "true", 
-	    	  	"content"=>$content,
-	    	  	"message"=>$content,
-	    	  	"url" => $url,
-	    	  	//"params" =>implode(",",array_keys($params)),
-	    	  	"params" =>serialize($params)
+            /*Logger::debug("Start wrokflow upfate",$products);
+            echo "Start wrokflow upfate".count($products).$products[2]->getMage_name();
+            die;*/
+            $returnValueContainer = new \Pimcore\Model\Tool\Admin\EventDataContainer(array());
 
-	    	  );
+            \Pimcore::getEventManager()->trigger('lpn.magento.postSynchro',$products,[
+                  "returnValueContainer" => $returnValueContainer
+              ]);
 
-       	  }
+            $workflowReturn = $returnValueContainer->getData();
+
+            if(is_array($workflowReturn) && isset($workflowReturn["message"])) {
+              $content .= $workflowReturn["message"];
+            }
+  	    	
+  	    	  $this->response = array(
+  	    	  	"success" => "true", 
+  	    	  	"content"=>$content,
+  	    	  	"message"=>$content,
+  	    	  	"url" => $url,
+  	    	  	//"params" =>implode(",",array_keys($params)),
+  	    	  	"params" =>serialize($params)
+
+  	    	  );
+
+      
        	  $this->_helper->json->sendJson($this->response);
        	  	
     	 

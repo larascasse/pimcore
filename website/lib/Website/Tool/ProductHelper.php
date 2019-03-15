46fe40acf4369af00163210985f3a186cc297c78
Florent Berenger <florent@lesmecaniques.net>
2019-03-15 10:03:36 +0100

    OPPS

--- website/lib/Website/Tool/ProductHelper.php
+++ website/lib/Website/Tool/ProductHelper.php
@@ -23,6 +23,9 @@ class ProductHelper
         $childConfigurableFields = array();
         $configurableFields =array("configurable_free_1","volume","subtype","finitionString","hauteur","profil","fixation","color","epaisseur","largeur","longueur","conditionnement","epaisseur_txt","largeur_txt","longueur_txt","mage_section","quantity_min_txt","configurable_free_2","choixString","traitement_surfaceString","motifString","supportString","chanfreins");
 
+        //Ajout pour gérer le choix non configureage
+        $configurableFields["choix"];
+
         
         $retrievableAttributes = [];
         foreach ($configurableFields as $key => $value) {

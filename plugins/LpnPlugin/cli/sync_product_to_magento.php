84e60247fb54ea3ba42b543c2122016f7f590c60
Florent Berenger <florent@lesmecaniques.net>
2019-04-10 12:35:27 +0200

    SYNC TIO PROD TMMP

--- plugins/LpnPlugin/cli/sync_product_to_magento.php
+++ plugins/LpnPlugin/cli/sync_product_to_magento.php
@@ -40,9 +40,15 @@ foreach ($results as $result) {
 
 echo count($eans)." à synchroniser ------- \n";
 if(count($eans)>0) {
+
+  //DEV
    $url = "http://shopdev.laparqueterienouvelle.fr/LPN/get_a_product_magmi.php";
-   //$params = ["ean"=>implode(",", $eans)];
    $params = ["ean"=>implode(",", $eans),"create"=>1];
+
+   //PROD
+   $url = "httpw://www.laparqueterienouvelle.fr/LPN/get_a_product_magmi.php";
+   $params = ["ean"=>implode(",", $eans)];
+
    print_r($params);
    $content = \Pimcore\Tool::getHttpData($url,null,$params);
     

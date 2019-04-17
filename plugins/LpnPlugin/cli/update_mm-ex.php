daaf19bd7e393169838d9912e78f93137c82d442
Florent Berenger <florent@lesmecaniques.net>
2019-04-17 11:13:08 +0200

    OPPS

--- plugins/LpnPlugin/cli/update_mm-ex.php
+++ plugins/LpnPlugin/cli/update_mm-ex.php
@@ -126,7 +126,7 @@ foreach ($list->getObjects() as $object) {
     
    if($isPointDeHongrie) {
 
-        $suffixeEan = $object->getEpaisseur().'x'.$object->getLargeur()."- PAT ".$object->getLongueur()." 45°";
+        $suffixeEan = $object->getEpaisseur().'x'.$object->getLargeur()."x".$object->getLongueur()."(PàT) 45°";
         $longueur_txt = 'Longueurs pointe à talon 510 mm';
     } 
 

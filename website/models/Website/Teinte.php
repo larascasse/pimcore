b4d98ec2b13b44987b7f7070ae6df21ab7311346
Florent Berenger <florent@lesmecaniques.net>
2019-05-02 12:09:29 +0200

    viuex bois

--- website/models/Website/Teinte.php
+++ website/models/Website/Teinte.php
@@ -327,6 +327,12 @@ class Website_Teinte extends Object_Teinte {
 	            $return["name"]  = "Bardage ".$return["name"];
 	            $return["pose"]  = $this->getPose();
 	            break;
+
+	          case 'bardage-vieux-bois':
+	            $return["short_name"]  = "Bardage vieux bois".$return["name"];
+	            $return["name"]  = "Bardage vieux bois".$return["name"];
+	            $return["pose"]  = $this->getPose();
+	            break;
 	        
 	        default:
 	             $return["short_name"]  = $return["name"];

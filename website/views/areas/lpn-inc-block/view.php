2e74d38d1f33deb8d5fc2ed6b6e0fd7ddeb06820
Florent Berenger <florent@lesmecaniques.net>
2019-06-03 14:30:02 +0200

    maj

--- website/views/areas/lpn-inc-block/view.php
+++ website/views/areas/lpn-inc-block/view.php
@@ -5,11 +5,12 @@
     ]); 
     $cssClass = $this->input("cssClass",["placeholder"=>"Classe css","htmlspecialchars"=>false]);
 
-    $cl = $cssClass->getData();
+      $cl = $cssClass->getData();
+      $cl = preg_replace('/\s+/', ' ', $cl);
       $cl = str_replace("&nbsp;", " ", $cl);
       $cl = str_replace(" ", " ", $cl);
       $cl = str_replace("&#160;", " ", $cl);
-      $cl = preg_replace('/\s+/', ' ', $cl);
+      
       
 
 

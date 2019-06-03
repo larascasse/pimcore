af259213ebad1df28c5e5e73e40bcc494951b14b
Florent Berenger <florent@lesmecaniques.net>
2019-06-03 13:32:03 +0200

    maj previeux

--- website/views/areas/lpn-inc-block/view.php
+++ website/views/areas/lpn-inc-block/view.php
@@ -4,6 +4,12 @@
         //"height" => 500
     ]); 
     $cssClass = $this->input("cssClass",["placeholder"=>"Classe css","htmlspecialchars"=>false]);
+
+    $cl = $cssClass->getData();
+      $cl = str_replace("&nbsp;", " ", $cl);
+      $cl = str_replace("&#160;", " ", $cl);
+
+
 //print_r(Pimcore\Tool::isFrontentRequestByAdmin());
   	if ($this->editmode) {
       echo $cssClass;
@@ -12,14 +18,13 @@
 
   	}
   	else if (Pimcore\Tool::isFrontentRequestByAdmin()) {
-
+      echo '<div id="pimbloc'.$doc->getElement()->getId().'" class="'.$cl.'">';
   		echo Document\Service::render($doc->getElement());
+      echo '</div>';
 
   	}	
   	else {
-      $cl = $cssClass->getData();
-      $cl = str_replace("&nbsp;", " ", $cl);
-      $cl = str_replace("&#160;", " ", $cl);
+      
       echo '<div id="pimbloc'.$doc->getElement()->getId().'" class="'.$cl.'">';
   		echo '{{widget type="cms/widget_block" template="cms/widget/static_block/default.phtml" block_id="'.$doc->getElement()->getKey().'"}}';
       echo '</div>';

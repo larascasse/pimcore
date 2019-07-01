483577e86608e0bb13ea49d19043a3b50b6ef067
Florent Berenger <florent@lesmecaniques.net>
2019-07-01 12:52:37 +0200

    opti

--- plugins/LpnPlugin/lib/LpnPlugin/Importer.php
+++ plugins/LpnPlugin/lib/LpnPlugin/Importer.php
@@ -1200,7 +1200,7 @@ use Pimcore\Model;
                                 $parentValue = $field->getForCsvExport($parent);
                                 //echo "checking Parent".$parentValue." child:".$value•"\n";
                                 if($parentValue != $value) {
-                                    echo "UPDATING PARENT ".$parent->getSku()." ".$parentValue." ".$value;
+                                    echo "UPDATING PARENT ".$parent->getSku()." ".$parentValue."->".$value."/n";
                                     $parent->setValue($key, $value);
                                     $parent->save();
                                 }
